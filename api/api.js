cc60bf65cdc642e9d1ad9db020225603667634f8
lucas <lucaspdroz@gmail.com>
2019-06-23 21:43:15 -0300

    adding API test - XHTTP will be update to fetch in a near future

--- api/api.js
+++ api/api.js
@@ -1,15 +1,55 @@
-let request = new XMLHttpRequest()
+function getGot() {
+    let request = new XMLHttpRequest()
+    request.open('GET', 'https://gameofthrones-df07d.firebaseio.com/personagens/-Lf2eQXFOcv0D8eqQbyn.json', true)
+    request.onload = function () {
+        let data = JSON.parse(this.response)
+        let personagens = data.personagens;
 
-request.open('GET', 'https://gameofthrones-df07d.firebaseio.com/personagens/-Lf2eQXFOcv0D8eqQbyn.json', true)
+        for (let ind in personagens) {
+            let li = document.createElement("li");
+            li.classList.add("course-wraper-item");
 
-request.onload = function () {
-    let data = JSON.parse(this.response)
-    let personagens = data.personagens;
+            let linha =
+            `   <p>${personagens[ind].nome} </p>
+                <p>${personagens[ind].tipo} <br> </p>
+            `;
+            li.innerHTML = linha;
+            document.querySelector('.course-wraper').appendChild(li);
+        }
 
-    for(let a in personagens){
-        console.log(personagens[a].nome);
-        console.log(personagens[a].tipo);
+        for (let a in personagens) {
+            document.querySelector('.body').innerHTML += personagens[a].nome + "&nbsp;";
+            document.querySelector('.body').innerHTML += personagens[a].tipo + "<br>";
+        }
     }
+
+    request.send()
 }
-// Send request
-request.send()
+
+async function getPersonage() {
+    let request = new XMLHttpRequest()
+    request.open('GET', 'https://gameofthrones-df07d.firebaseio.com/personagens/-Lf2eQXFOcv0D8eqQbyn/personagens.json', true)
+
+    request.onload = function () {
+        let data = JSON.parse(this.response)
+        console.log(data);
+        let course = data.cursosEmDestaque;
+
+        for (let ind in course) {
+            let li = document.createElement("li");
+            li.classList.add("course-wraper-item");
+
+            let linha =
+            `   <p>${course[ind].nome}</p>
+                <p>${course[ind].tipo}</p>
+            `;
+            
+            li.innerHTML = linha;
+            document.querySelector('.course-wraper').appendChild(li);
+        }
+    }
+    request.send()
+}
+getGot()
+
+getPersonage()
---
+++ api/firebase.js
@@ -0,0 +1,15 @@
+// Anonymous function to protect the data
+(function () {
+    // Your web app's Firebase configuration
+    var firebaseConfig = {
+        apiKey: "AIzaSyAmxtBH9FvW3bWP6pioFc2PkYFZ9baRdFk",
+        authDomain: "cantinho-dos-travessos.firebaseapp.com",
+        databaseURL: "https://cantinho-dos-travessos.firebaseio.com",
+        projectId: "cantinho-dos-travessos",
+        storageBucket: "cantinho-dos-travessos.appspot.com",
+        messagingSenderId: "231856558202",
+        appId: "1:231856558202:web:315c3488e7ce3700"
+    };
+    // Initialize Firebase
+    firebase.initializeApp(firebaseConfig);
+})()
