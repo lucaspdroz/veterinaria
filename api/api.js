22fb54f0d991fcd6fb609de0a8d03bec7c44be7d
lucas <lucaspdroz@gmail.com>
2019-06-29 16:04:54 -0300

    fetch api data GET

--- api/api.js
+++ api/api.js
@@ -1,55 +1,92 @@
-function getGot() {
-    let request = new XMLHttpRequest()
-    request.open('GET', 'https://gameofthrones-df07d.firebaseio.com/personagens/-Lf2eQXFOcv0D8eqQbyn.json', true)
-    request.onload = function () {
-        let data = JSON.parse(this.response)
-        let personagens = data.personagens;
+// function getGot() {
+//     let request = new XMLHttpRequest()
+//     request.open('GET', 'https://gameofthrones-df07d.firebaseio.com/personagens/-Lf2eQXFOcv0D8eqQbyn.json', true)
+//     request.onload = function () {
+//         let data = JSON.parse(this.response)
+//         let personagens = data.personagens;
 
-        for (let ind in personagens) {
-            let li = document.createElement("li");
-            li.classList.add("course-wraper-item");
+//         for (let ind in personagens) {
+//             let li = document.createElement("li");
+//             li.classList.add("clientes-wraper-item");
 
-            let linha =
-            `   <p>${personagens[ind].nome} </p>
-                <p>${personagens[ind].tipo} <br> </p>
-            `;
-            li.innerHTML = linha;
-            document.querySelector('.course-wraper').appendChild(li);
-        }
+//             let linha =
+//             `   <p>${personagens[ind].nome} </p>
+//                 <p>${personagens[ind].tipo} <br> </p>
+//             `;
+//             li.innerHTML = linha;
+//             document.querySelector('.clientes-wraper').appendChild(li);
+//         }
 
-        for (let a in personagens) {
-            document.querySelector('.body').innerHTML += personagens[a].nome + "&nbsp;";
-            document.querySelector('.body').innerHTML += personagens[a].tipo + "<br>";
-        }
-    }
+//         for (let a in personagens) {
+//             document.querySelector('.body').innerHTML += personagens[a].nome + "&nbsp;";
+//             document.querySelector('.body').innerHTML += personagens[a].tipo + "<br>";
+//         }
+//     }
 
-    request.send()
-}
+//     request.send()
+// }
 
-async function getPersonage() {
-    let request = new XMLHttpRequest()
-    request.open('GET', 'https://gameofthrones-df07d.firebaseio.com/personagens/-Lf2eQXFOcv0D8eqQbyn/personagens.json', true)
+// async function getPersonage() {
+//     let request = new XMLHttpRequest()
+//     request.open('GET', 'https://gameofthrones-df07d.firebaseio.com/personagens/-Lf2eQXFOcv0D8eqQbyn/personagens.json', true)
 
-    request.onload = function () {
-        let data = JSON.parse(this.response)
-        console.log(data);
-        let course = data.cursosEmDestaque;
+//     request.onload = function () {
+//         let data = JSON.parse(this.response)
+//         console.log(data);
+//         let clientes = data.cursosEmDestaque;
 
-        for (let ind in course) {
-            let li = document.createElement("li");
-            li.classList.add("course-wraper-item");
+//         for (let ind in clientes) {
+//             let li = document.createElement("li");
+//             li.classList.add("clientes-wraper-item");
 
-            let linha =
-            `   <p>${course[ind].nome}</p>
-                <p>${course[ind].tipo}</p>
-            `;
+//             let linha =
+//             `   <p>${clientes[ind].nome}</p>
+//                 <p>${clientes[ind].tipo}</p>
+//             `;
             
-            li.innerHTML = linha;
-            document.querySelector('.course-wraper').appendChild(li);
-        }
-    }
-    request.send()
-}
-getGot()
+//             li.innerHTML = linha;
+//             document.querySelector('.clientes-wraper').appendChild(li);
+//         }
+//     }
+//     request.send()
+// }
+// getGot()
 
-getPersonage()
+// getPersonage()
+
+
+
+let url = "https://cantinho-dos-travessos.firebaseio.com/cliente.json?nome";
+
+fetch(url)
+  .then(function(response) {
+    return response.json();
+  })
+  .then(function(myJson) {
+    //   após myJson (cursosEmDestaque) é o nome do objeto Pai
+    let clientes = myJson;
+    console.log(clientes);
+    
+    for(let i in clientes){
+        console.log(clientes[i]);
+        console.log(clientes[i].email);
+    }
+
+    // for (let ind in clientes) {
+      // cria uma <li>
+    //   let li = document.createElement("li");
+      // cria dentro da <li> 
+    //   li.classList.add("clientes-wraper-item");
+  
+      //   Definição do elemento
+    //   Objeto dinâmico apartir do "campo_do_obejo[indice].nome_do_campo"
+    //   let linha =
+    //     ` <img class="icones" src="${clientes[ind].Whatsapp}" alt="logo para ${clientes[ind].name_dono}"/>
+    //       <p>${clientes[ind].nome}</p>
+    //       <a class="link-button" href="${clientes[ind].url}">Acesse já</a>
+    //     `;
+        // adiciona linha dentro da <li>
+    //   li.innerHTML = linha;
+        //  adiciona tudo dentro da classe "clientes-wraper" apendendo na última <li>
+    //   document.querySelector('.clientes-wraper').appendChild(li);
+  });
