262a4e61988a985c023debd06813e451e70fd8fa
Giovani Boff <giovanisouza15@gmail.com>
2019-06-29 20:43:48 -0300

    add funcao de busca do input

--- src/components/search-menu/js/main.js
+++ src/components/search-menu/js/main.js
@@ -1,1 +1,35 @@
-console.log("JS funcionando");
+// console.log("JS funcionando");
+const searchBar = document.querySelector('#searchBar');
+const list = document.querySelector("#ul-search");
+searchBar.addEventListener('keyup',e=>{
+
+    const term = e.target.value.toLowerCase();
+    const langs = list.getElementsByTagName('li');
+    Array.from(langs).forEach((lang)=>{
+        const name = lang.firstElementChild.textContent;
+        if(name.toLowerCase().indexOf(term)!= -1){
+            lang.style.display = 'block';
+        }else{
+            lang.style.display = 'none';
+        }
+    })
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  
+  
