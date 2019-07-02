6d48431533102b085c0e625bd6c5b86b100103fe
Giovani Boff <giovanisouza15@gmail.com>
2019-07-02 16:50:52 -0300

    added search bar working,needs to be styled

--- api/api.js
+++ api/api.js
@@ -1,15 +1,21 @@
-let request = new XMLHttpRequest()
 
-request.open('GET', 'https://gameofthrones-df07d.firebaseio.com/personagens/-Lf2eQXFOcv0D8eqQbyn.json', true)
-
-request.onload = function () {
-    let data = JSON.parse(this.response)
-    let personagens = data.personagens;
-
-    for(let a in personagens){
-        console.log(personagens[a].nome);
-        console.log(personagens[a].tipo);
+// GET()
+let url = "https://cantinho-dos-travessos.firebaseio.com/cliente.json?nome";
+fetch(url)
+  .then(response =>{
+    return response.json();
+  })
+  .then(myJson =>{
+    //   após myJson (cursosEmDestaque) é o nome do objeto Pai
+    let clientes = myJson;
+    console.log(clientes);
+    
+    for(let i in clientes){
+        console.log(clientes[i]);
+        let li = document.createElement("li");
+        li.innerHTML =`
+          <a href="#">${clientes[i].nome_cliente}</a>
+          `;
+        list.appendChild(li);
     }
-}
-// Send request
-request.send()
+  });
