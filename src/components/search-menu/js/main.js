45961accea363d46e7a202213fd202ef4721138e
Giovani Boff <giovanisouza15@gmail.com>
2019-07-07 14:27:36 -0300

    modifying GET api to show the names of pets at main compinent

--- src/components/search-menu/js/main.js
+++ src/components/search-menu/js/main.js
@@ -2,7 +2,6 @@
 const searchBar = document.querySelector('#search-bar');
 const list = document.querySelector("#ul-search");
 searchBar.addEventListener('keyup', e => {
-    console.log(e);
 
     const term = e.target.value.toLowerCase();
     const users = list.getElementsByTagName('li');
@@ -32,20 +31,20 @@ fetch(url)
         // console.log(clientes);
         for (let i in clientes) {
 
-            // console.log(clientes[i]);
+            console.log(clientes[i]);
             let li = document.createElement("li");
             // li.classList.add('lista');
             li.innerHTML =
                 `
-          <div onClick ='bodyContent("${clientes[i].nome_cliente}","${clientes[i].telefone}","${clientes[i].celular}","${clientes[i].email}","${clientes[i].whatsapp}")'>
-          <a href="#" >${clientes[i].nome_cliente}</a>
-          </div>
+          
+          <a href="#" onClick ='bodyContent("${clientes[i].name_pet}","${clientes[i].description_pet}","${clientes[i].nome_cliente}","${clientes[i].telefone}","${clientes[i].celular}","${clientes[i].email}","${clientes[i].whatsapp}")'>${clientes[i].nome_cliente}</a>
+         
           
         `;
             list.appendChild(li);
         }
     });
-const ownerContainer = document.querySelector('#owner-container');
+const petBox = document.querySelector('#pet-box');
 const ownerBox = document.querySelector('#owner-box');
 const h1 = document.createElement("h1");
 const h2 = document.createElement("h2");
@@ -54,19 +53,26 @@ const span1 = document.createElement("span");
 const span2 = document.createElement("span");
 const span3 = document.createElement("span");
 const span4 = document.createElement("span");
-const bodyContent = (name, phone1, phone2, email, zap) => {
+const bodyContent = (namePet,descriptionPet,nameOwner, phone1, phone2, email, zap) => {
+    h1.innerHTML = ` `;
     h2.innerHTML = ` `;
+    h4.innerHTML = ` `;
     span1.innerHTML = ` `;
     span2.innerHTML = ` `;
     span3.innerHTML = ` `;
     span4.innerHTML = ` `;
 
-    h2.innerHTML = `${name}`;
-    span1.innerHTML = ` ${phone1}`;
-    span2.innerHTML = `${phone2}`;
-    span3.innerHTML = `${email} `;
-    span4.innerHTML = `${zap}`;
 
+    h1.innerHTML = `Pet: ${namePet}`;
+    h2.innerHTML = `Dono: ${nameOwner}`;
+    h4.innerHTML = `Descrição: ${descriptionPet}`;
+    span1.innerHTML = `Telefone: ${phone1}`;
+    span2.innerHTML = `Celular: ${phone2}`;
+    span3.innerHTML = `Email: ${email} `;
+    span4.innerHTML = `Whatspp: ${zap}`;
+
+    petBox.appendChild(h1);
+    petBox.appendChild(h4);
     ownerBox.appendChild(h2);
     ownerBox.appendChild(span1);
     ownerBox.appendChild(span2);
